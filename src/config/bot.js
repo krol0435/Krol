86658df2da82a3e4ab44671bb364344e46ccafa2
krol0435 <b75njymyg5@privaterelay.appleid.com>
2026-07-07 18:34:27 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Krol Bot").
-        name: "Made with Love by krol0435",
+        name: "by krol0435",
         // Activity type number (0 = Playing).
         type: 4,
       },
